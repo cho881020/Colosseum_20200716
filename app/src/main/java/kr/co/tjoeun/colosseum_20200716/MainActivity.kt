b2294114618ac766d1df6897dac401e9b6f95ccf
조경진 <kj_cho@idealidea.co.kr>
2020-07-16 21:41:32 +0900

    로그인 버튼 누르면 실제 입력한 값으로 로그인 시도

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/MainActivity.kt
@@ -2,6 +2,8 @@ package kr.co.tjoeun.colosseum_20200716
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.colosseum_20200716.utils.ServerUtil
 
 class MainActivity : BaseActivity() {
 
@@ -13,7 +15,16 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        loginBtn.setOnClickListener {
 
+//            입력한 아이디와 비번을 받아서
+            val inputEmail = emailEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+
+//            서버에 전달해주고 응답 처리
+            ServerUtil.postRequestLogin(mContext, inputEmail, inputPw, null)
+
+        }
     }
 
     override fun setValues() {
