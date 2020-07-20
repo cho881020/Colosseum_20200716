d188702dbb3f837735e08c1efe9410e7358426f8
조경진 <kj_cho@idealidea.co.kr>
2020-07-20 19:44:26 +0900

    비밀번호 EditText 내용 변경 이벤트 추가

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/SignUpActivity.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/SignUpActivity.kt
@@ -2,6 +2,10 @@ package kr.co.tjoeun.colosseum_20200716
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +16,25 @@ class SignUpActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+//        비밀번호 입력 내용 변경 이벤트 처리
+        passwordEdt.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//                내용 변경 완료된 시점에 실행
+
+                Log.d("비번입력", s.toString())
+
+            }
+
+        })
 
     }
 
