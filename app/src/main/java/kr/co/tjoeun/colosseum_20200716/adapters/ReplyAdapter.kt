9b6e160ec03539cf7fa7959ca3b8708f539a9ba4
조경진 <kj_cho@idealidea.co.kr>
2020-07-22 21:19:16 +0900

    의견 목록 어댑터 기초 작업

---
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/adapters/ReplyAdapter.kt
@@ -0,0 +1,30 @@
+package kr.co.tjoeun.colosseum_20200716.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kr.co.tjoeun.colosseum_20200716.R
+import kr.co.tjoeun.colosseum_20200716.datas.Reply
+
+class ReplyAdapter(
+    val mContext:Context,
+    resId: Int,
+    val mList:List<Reply>) : ArrayAdapter<Reply>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.reply_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+
+}
