9a6aeb1385e06f62197b6e9fb2afd19860976117
조경진 <kj_cho@idealidea.co.kr>
2020-07-23 19:46:14 +0900

    의견 남기기 - 서버 기능 사용

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/EditReplyActivity.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/EditReplyActivity.kt
@@ -3,8 +3,13 @@ package kr.co.tjoeun.colosseum_20200716
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_edit_reply.*
+import kr.co.tjoeun.colosseum_20200716.utils.ServerUtil
+import org.json.JSONObject
 
 class EditReplyActivity : BaseActivity() {
+
+//    어떤 토론에 대한 의견을 다는지 알려주는 멤버변수
+    var mTopicId = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,12 +20,30 @@ class EditReplyActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        postBtn.setOnClickListener {
+
+//            입력 내용 저장
+            val inputContent = contentEdt.text.toString()
+
+            ServerUtil.postRequestReply(mContext, mTopicId, inputContent, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+
+                }
+
+            })
+
+        }
+
     }
 
     override fun setValues() {
 
         topicTitleTxt.text = intent.getStringExtra("topicTitle")
         mySideTitleTxt.text = intent.getStringExtra("selectedSideTitle")
+
+//        몇번 토론에 대한 의견 작성인지 변수로 저장
+        mTopicId = intent.getIntExtra("topicId", 0)
+
 
     }
 
