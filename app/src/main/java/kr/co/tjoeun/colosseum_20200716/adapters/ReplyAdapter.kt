c09e55f31962c3d3fffe93e43c2117d32836a2e4
조경진 <kj_cho@idealidea.co.kr>
2020-07-27 22:09:13 +0900

    좋아요 버튼 이벤트처리

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/adapters/ReplyAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/adapters/ReplyAdapter.kt
@@ -2,16 +2,21 @@ package kr.co.tjoeun.colosseum_20200716.adapters
 
 import android.content.Context
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import kr.co.tjoeun.colosseum_20200716.R
 import kr.co.tjoeun.colosseum_20200716.ViewReplyDetailActivity
 import kr.co.tjoeun.colosseum_20200716.datas.Reply
+import kr.co.tjoeun.colosseum_20200716.utils.ServerUtil
 import kr.co.tjoeun.colosseum_20200716.utils.TimeUtil
+import org.json.JSONObject
 import java.text.SimpleDateFormat
 
 class ReplyAdapter(
@@ -81,6 +86,56 @@ class ReplyAdapter(
             mContext.startActivity(myIntent)
 
         }
+
+
+//        의견에 대한 좋아요 / 싫어요 버튼 클릭 이벤트
+
+        likeBtn.setOnClickListener {
+
+            ServerUtil.postRequestReplyLikeOrDislike(mContext, data.id, true, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+
+//                    변경된 좋아요 갯수 / 싫어요 갯수를 파악해서
+//                    버튼 문구를 새로 반영
+//                     => 목록에 뿌려지는 data의 좋아요/싫어요 갯수를 변경
+
+                    val dataObj = json.getJSONObject("data")
+                    val replyObj = dataObj.getJSONObject("reply")
+
+                    val reply = Reply.getReplyFromJson(replyObj)
+
+//                    이미 화면에 뿌려져 있는 data의 내용만 교체
+                    data.likeCount = reply.likeCount
+
+//                    data의 값이 변경 => 리스트뷰를 구성하는 목록에 변경
+//                    => 어댑터.notifyDataSetChanged 실행해야함
+//                    => 이 코드는 어댑터 내부 => notifyDataSetChanged 가 내장되어 있다.
+//                    => 단순히 호출만 하면 끝.
+
+//                    새로고침도 => UI 변경 => runOnUiThread 등으로 UI쓰레드가 처리하도록 해야함.
+//                    어댑터는 runOnUiThread 기능이 내장되어 있지 않다.
+
+//                    Handler 를 이용해서 => UI쓰레드에 접근하게 하자.
+
+                    val uiHandler = Handler(Looper.getMainLooper())
+
+                    uiHandler.post {
+                        notifyDataSetChanged()
+
+//                        서버가 알려주는 메세지를 토스트로 출력
+                        val message = json.getString("message")
+
+                        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+
+                    }
+
+
+                }
+
+            })
+
+        }
+
 
         return row
     }
