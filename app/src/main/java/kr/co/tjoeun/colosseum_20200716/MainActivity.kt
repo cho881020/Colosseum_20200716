8dc7c9352ae6cb524974021c1e70d618bd6164bf
조경진 <kj_cho@idealidea.co.kr>
2020-07-29 20:35:30 +0900

    모든 액티비티가 => 커스텀 액션바를 사용하도록 설정  -> supportActionBar가 있는 액티비티만 사용하도록. let 활용

--- app/src/main/java/kr/co/tjoeun/colosseum_20200716/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/MainActivity.kt
@@ -20,7 +20,6 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
         setupEvents()
         setValues()
-        setCustomActionBar()
     }
 
     override fun setupEvents() {
